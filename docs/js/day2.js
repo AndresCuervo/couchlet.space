075e04f4c6ae7a2d7eff657196324ce5ff00db0d
Andres Cuervo <andrescuervor@gmail.com>
2017-05-12 11:02:22 -0400

    Add gui and no-repeat main bg

--- docs/js/day2.js
+++ docs/js/day2.js
@@ -1,4 +1,3 @@
-
 if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
 
 var renderer, scene, camera, stats, effect;
@@ -10,7 +9,7 @@ var particles = 1000;
 var WIDTH = window.innerWidth;
 var HEIGHT = window.innerHeight;
 
-var flashPoints;
+var flashPoints = { 'on' : false };
 
 var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
@@ -23,60 +22,60 @@ var geo;
 
 function init() {
 
-	camera = new THREE.PerspectiveCamera( 40, WIDTH / HEIGHT, 1, 10000 );
-	camera.position.z = 300;
+    camera = new THREE.PerspectiveCamera( 40, WIDTH / HEIGHT, 1, 10000 );
+    camera.position.z = 300;
 
-	controls = new THREE.TrackballControls( camera );
-	controls.rotateSpeed = 1.0;
-	controls.zoomSpeed = 1.2;
-	controls.panSpeed = 0.8;
-	controls.noZoom = false;
-	controls.noPan = false;
-	controls.staticMoving = true;
-	controls.dynamicDampingFactor = 0.3;
-	controls.keys = [ 65, 83, 68 ];
-	scene = new THREE.Scene();
+    controls = new THREE.TrackballControls( camera );
+    controls.rotateSpeed = 1.0;
+    controls.zoomSpeed = 1.2;
+    controls.panSpeed = 0.8;
+    controls.noZoom = false;
+    controls.noPan = false;
+    controls.staticMoving = true;
+    controls.dynamicDampingFactor = 0.3;
+    controls.keys = [ 65, 83, 68 ];
+    scene = new THREE.Scene();
 
     var g;
     var loader = new THREE.PLYLoader();
     loader.load( 'assets/models/moss-garden-cloud-tiny-2.ply', function ( geometry ) {
 
 
-		uniforms = {
+        uniforms = {
 
-			color:     { value: new THREE.Color( 0xffffff ) },
-			texture:   { value: new THREE.TextureLoader().load( "textures/spark.png" ) }
+            color:     { value: new THREE.Color( 0xffffff ) },
+            texture:   { value: new THREE.TextureLoader().load( "textures/spark.png" ) }
 
-		};
+        };
 
-		var shaderMaterial = new THREE.ShaderMaterial( {
+        var shaderMaterial = new THREE.ShaderMaterial( {
 
-			uniforms:       uniforms,
-			vertexShader:   document.getElementById( 'vertexshader' ).textContent,
-			fragmentShader: document.getElementById( 'fragmentshader' ).textContent,
+            uniforms:       uniforms,
+            vertexShader:   document.getElementById( 'vertexshader' ).textContent,
+            fragmentShader: document.getElementById( 'fragmentshader' ).textContent,
 
-			blending:       THREE.NormalBlending,
-			depthTest:      true,
-			transparent:    true,
+            blending:       THREE.NormalBlending,
+            depthTest:      true,
+            transparent:    true,
 
 
-		});
+        });
 
         g = geometry.attributes.position.array;
-		c = geometry.attributes.color.array;
+        c = geometry.attributes.color.array;
 
-		console.log(geometry);
+        console.log(geometry);
         particles = g.length / 3;
 
-		var radius = 3;
+        var radius = 3;
 
-		geometry = new THREE.BufferGeometry();
+        geometry = new THREE.BufferGeometry();
 
-		var positions = new Float32Array( particles * 3 );
-		var colors = new Float32Array( particles * 3 );
-		var sizes = new Float32Array( particles );
+        var positions = new Float32Array( particles * 3 );
+        var colors = new Float32Array( particles * 3 );
+        var sizes = new Float32Array( particles );
 
-		var color = new THREE.Color();
+        var color = new THREE.Color();
 
         var scale = 800;
         for ( var i = 0; i < positions.length; i += 3 ) {
@@ -90,28 +89,28 @@ function init() {
             positions[ i + 1 ] = y * scale;
             positions[ i + 2 ] = z * scale;
 
-			colors[ i + 0 ] = c[ i ];
-			colors[ i + 1 ] = c[ i + 1];
-			colors[ i + 2 ] = c[ i + 2];
+            colors[ i + 0 ] = c[ i ];
+            colors[ i + 1 ] = c[ i + 1];
+            colors[ i + 2 ] = c[ i + 2];
 
-			sizes[i/3] = 1;
+            sizes[i/3] = 1;
         }
 
-		geometry.addAttribute( 'position', new THREE.BufferAttribute( positions, 3 ) );
-		geometry.addAttribute( 'customColor', new THREE.BufferAttribute( colors, 3 ) );
-		geometry.addAttribute( 'size', new THREE.BufferAttribute( sizes, 1 ) );
+        geometry.addAttribute( 'position', new THREE.BufferAttribute( positions, 3 ) );
+        geometry.addAttribute( 'customColor', new THREE.BufferAttribute( colors, 3 ) );
+        geometry.addAttribute( 'size', new THREE.BufferAttribute( sizes, 1 ) );
 
-		particleSystem = new THREE.Points( geometry, shaderMaterial );
+        particleSystem = new THREE.Points( geometry, shaderMaterial );
 
-		scene.add( particleSystem );
+        scene.add( particleSystem );
 
-	    geo = geometry;
-		
+        geo = geometry;
 
-		var width = window.innerWidth || 2;
-		var height = window.innerHeight || 2;
-		effect = new THREE.AnaglyphEffect( renderer );
-		effect.setSize( width, height )
+
+        var width = window.innerWidth || 2;
+        var height = window.innerHeight || 2;
+        effect = new THREE.AnaglyphEffect( renderer );
+        effect.setSize( width, height )
     } );
 
     renderer = new THREE.WebGLRenderer( { antialias: false } );
@@ -119,99 +118,80 @@ function init() {
     renderer.setSize( window.innerWidth, window.innerHeight );
 
     container.appendChild( renderer.domElement );
-	
-    var checkbox = document.createElement('input');
-    var checkboxID = "flashPoints"
-                 checkbox.type = "checkbox";
-                 checkbox.name = "flash-points-checkbox";
-                 checkbox.value = flashPoints;
-                 checkbox.id = checkboxID;
-                 checkbox.onclick = function () {
-                     checkbox.value = !flashPoints;
-                     flashPoints = !flashPoints;
-                     points.material.size = 0.5;
-                 };
-
-                 var label = document.createElement('label')
-                 label.htmlFor = checkboxID;
-                 label.appendChild(document.createTextNode('Anaglyph'));
-
-                 checkbox.style = "position: fixed; left: 100px; top: 1%;";
-                 label.style = "position: fixed; left: 120px; top: 1%; color: white;";
-
-                 container.appendChild(checkbox);
-                 container.appendChild(label);
-    //
 
     stats = new Stats();
     container.appendChild( stats.dom );
 
-    window.addEventListener( 'resize', onWindowResize, false );
+    window.onload = function() {
+        var gui = new dat.GUI();
+        gui.add(flashPoints, 'on');
+    };
 
+    window.addEventListener( 'resize', onWindowResize, false );
 }
 
 function onWindowResize() {
 
-	camera.aspect = window.innerWidth / window.innerHeight;
-	camera.updateProjectionMatrix();
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
 
-	renderer.setSize( window.innerWidth, window.innerHeight );
-	
-	effect.setSize( window.innerWidth, window.innerHeight );
-	
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+    effect.setSize( window.innerWidth, window.innerHeight );
+
 
 }
 
 function onDocumentMouseMove(event) {
-	mouseX = ( event.clientX - windowHalfX ) / 100;
-	mouseY = ( event.clientY - windowHalfY ) / 100;
+    mouseX = ( event.clientX - windowHalfX ) / 100;
+    mouseY = ( event.clientY - windowHalfY ) / 100;
 }
 
 
 function animate() {
 
-	requestAnimationFrame( animate );
+    requestAnimationFrame( animate );
 
-	render();
-	stats.update();
-	controls.update();
+    render();
+    stats.update();
+    controls.update();
 
 }
 
 function render() {
-	
-	// var x = event.clientX;
-	// var y = event.clientY;
-	//
-	// console.log(x, y);
 
-	var time = Date.now() * 0.005;
+    // var x = event.clientX;
+    // var y = event.clientY;
+    //
+    // console.log(x, y);
 
-    
-
-	var range = .1;
-	if (!!geo){
-		var sizes = geo.attributes.size.array;
-		var positions = geo.attributes.position.array;
-
-		for ( var i = 0; i < particles; i++ ) {
+    var time = Date.now() * 0.005;
 
 
-			//sizes[ i ] = 10 * ( 1 + Math.sin(  i + time ) );
-			positions[i*3 + 0] = positions[i*3 + 0] + 
-				((time+positions[i*3+1]*.1) % (range) - (range/2.0));
 
-		}
+    var range = .1;
+    if (!!geo){
+        var sizes = geo.attributes.size.array;
+        var positions = geo.attributes.position.array;
 
-		geo.attributes.position.needsUpdate = true;
-	}
+        for ( var i = 0; i < particles; i++ ) {
 
-	if (flashPoints){
-		effect.render( scene, camera );
-	} else {
-		renderer.render( scene, camera );
-	}
-	
+
+            //sizes[ i ] = 10 * ( 1 + Math.sin(  i + time ) );
+            positions[i*3 + 0] = positions[i*3 + 0] +
+                ((time+positions[i*3+1]*.1) % (range) - (range/2.0));
+
+        }
+
+        geo.attributes.position.needsUpdate = true;
+    }
+
+    if (flashPoints.on){
+        effect.render( scene, camera );
+    } else {
+        renderer.render( scene, camera );
+    }
+
 
 }
 
