87fe4f3b9be1ee88c476e341f15710c551ef4804
sjenson <sjenson@oberlin.edu>
2017-05-16 00:21:11 -0400

    Ur banishd hugo

---
+++ content/day1.md
@@ -0,0 +1,6 @@
+---
+title: 05.10.2017
+display: pointcloud
+---
+
+<script src="../js/day1.js"></script>
--- docs/js/day1.js
+++ docs/js/day1.js
@@ -1,5 +1,3 @@
-/* couchlet day 1 */
-
 if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
 
 var container, stats;
@@ -8,57 +6,14 @@ var camera, scene, renderer;
 
 var points;
 
-function loaderGuts( geometry ){
-    geometry.computeVertexNormals();
-
-    g = geometry.attributes.position.array;
-
-    var particles = g.length / 3;
-
-    var geometry = new THREE.BufferGeometry();
-
-    var positions = new Float32Array( particles * 3 );
-    var colors = new Float32Array( particles * 3 );
-
-    var color = new THREE.Color();
-
-    var scale = 300;
-    for ( var i = 0; i < positions.length; i += 3 ) {
-
-        for (var j = 0; j < 3; j++){
-            var x = g[i + j];
-            positions[i + j] = x * scale;
-            colors[i + j] = 1;
-        }
-    }
-
-    geometry.addAttribute( 'position', new THREE.BufferAttribute( positions, 3 ) );
-    geometry.addAttribute( 'color', new THREE.BufferAttribute( colors, 3 ) );
-
-    geometry.computeBoundingSphere();
-
-    var material = new THREE.PointsMaterial( { size: 15, vertexColors: THREE.VertexColors } );
-
-    points = new THREE.Points( geometry, material );
-
-    scene.add( points );
-
-    renderer = new THREE.WebGLRenderer( { antialias: false } );
-    renderer.setClearColor( scene.fog.color );
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-
-    container.appendChild( renderer.domElement );
-
-    stats = new Stats();
-    container.appendChild( stats.dom );
-
-    window.addEventListener('resize', onWindowResize, false );
-}
+init();
+animate();
 
 function init() {
 
     container = document.getElementById( 'container' );
+
+    //
 
     camera = new THREE.PerspectiveCamera( 27, window.innerWidth / window.innerHeight, 5, 3500 );
     camera.position.z = 2750;
@@ -66,8 +21,98 @@ function init() {
     scene = new THREE.Scene();
     scene.fog = new THREE.Fog( 0x050505, 2000, 3500 );
 
+    //
+    var g;
     var loader = new THREE.PLYLoader();
-    loader.load( 'assets/models/kinectCloud.ply', loaderGuts);
+    loader.load( '../assets/models/kinectCloud.ply', function ( geometry ) {
+
+        geometry.computeVertexNormals();
+
+        console.log("geometry is:");
+        g = geometry.attributes.position.array;
+        console.log("g: ", g.length);
+        // for ()
+
+        // var material = new THREE.MeshStandardMaterial( { color: 0x0055ff, shading: THREE.FlatShading } );
+        // var mesh = new THREE.Mesh( geometry, material );
+        //
+        // mesh.position.y = - 0.2;
+        // mesh.position.z =   0.3;
+        // mesh.rotation.x = - Math.PI / 2;
+        // // mesh.scale.multiplyScalar( 0.001 );
+        //
+        // mesh.castShadow = true;
+        // mesh.receiveShadow = true;
+        //
+        // scene.add( mesh );
+        var particles = g.length / 3;
+
+        var geometry = new THREE.BufferGeometry();
+
+        var positions = new Float32Array( particles * 3 );
+        var colors = new Float32Array( particles * 3 );
+
+        var color = new THREE.Color();
+
+        // var n = 1000, n2 = n / 2; // particles spread in the cube
+
+        var scale = 300;
+        for ( var i = 0; i < positions.length; i += 3 ) {
+
+            // positions
+            var x = g[i];
+            var y = g[i+1];
+            var z = g[i+2];
+
+            positions[ i ]     = x * scale;
+            positions[ i + 1 ] = y * scale;
+            positions[ i + 2 ] = z * scale;
+
+            // colors
+
+            // var vx = ( x / n ) + 0.5;
+            // var vy = ( y / n ) + 0.5;
+            // var vz = ( z / n ) + 0.5;
+
+            color.setRGB( 1, 1, 1 );
+
+            colors[ i ]     = color.r;
+            colors[ i + 1 ] = color.g;
+            colors[ i + 2 ] = color.b;
+
+        }
+
+        geometry.addAttribute( 'position', new THREE.BufferAttribute( positions, 3 ) );
+        geometry.addAttribute( 'color', new THREE.BufferAttribute( colors, 3 ) );
+
+        geometry.computeBoundingSphere();
+
+        //
+
+        var material = new THREE.PointsMaterial( { size: 15, vertexColors: THREE.VertexColors } );
+
+        points = new THREE.Points( geometry, material );
+
+        scene.add( points );
+
+        renderer = new THREE.WebGLRenderer( { antialias: false } );
+        renderer.setClearColor( scene.fog.color );
+        renderer.setPixelRatio( window.devicePixelRatio );
+        renderer.setSize( window.innerWidth, window.innerHeight );
+
+        container.appendChild( renderer.domElement );
+
+        //
+
+        stats = new Stats();
+        container.appendChild( stats.dom );
+
+        //
+
+        window.addEventListener( 'resize', onWindowResize, false );
+
+    } );
+
 }
 
 function onWindowResize() {
@@ -76,7 +121,10 @@ function onWindowResize() {
     camera.updateProjectionMatrix();
 
     renderer.setSize( window.innerWidth, window.innerHeight );
+
 }
+
+//
 
 function animate() {
 
@@ -89,7 +137,6 @@ function animate() {
 }
 
 function render() {
-
     if (!!points) {
         var time = Date.now() * 0.001;
 
@@ -99,5 +146,4 @@ function render() {
     }
 }
 
-init();
-animate();
+
